9058601c5c23bd1f9d17e0ac2de8361e4a1a0180
Fattis <malte@dellby.eu>
2024-11-22 20:01:44 +0100

    testing script

--- backend/Vector_database_backend.py
+++ backend/Vector_database_backend.py
@@ -1,15 +1,18 @@
 import ollama
 import chromadb
+from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 from chromadbx import ULIDGenerator
 
 
 class vector_data_base:
     def __init__(self):
-        self.SSID_vectordatabase = {}
-
-        self.client = (
-            chromadb.Client()
+        self.client = chromadb.PersistentClient(
+            path="../databases",  # this doesn't do what it should, but still works????
+            settings=Settings(),
+            tenant=DEFAULT_TENANT,
+            database=DEFAULT_DATABASE,
         )  # probably should be a persistent client but I digress
+        self.SSID_vectordatabase = {}
 
     def vectorize_string(
         self, string: str, SSID: str, name: str, split: int = 1000, overlap: int = 250
@@ -17,7 +20,9 @@ class vector_data_base:
         asdf = True
         """sttring should be the whole document as a string, SSID for the session ID, split is the amount oof chars in a single embedding, overlap is the amount of overlap in chars inm each embedding"""
         if not SSID in self.SSID_vectordatabase:
-            self.SSID_vectordatabase[SSID] = self.client.create_collection(name=SSID)
+            self.SSID_vectordatabase[SSID] = self.client.get_or_create_collection(
+                name=SSID
+            )
         embedding = []
         test_eqvuilant = []
         for _, j in enumerate(
@@ -49,13 +54,23 @@ class vector_data_base:
 
     def vector_retrieve(self, prompt: str, SSID: str, n_results: int = 1) -> list:
         if not SSID in self.SSID_vectordatabase:
-            raise Exception("No data by that SSID")
+            self.SSID_vectordatabase[SSID] = self.client.get_collection(name=SSID)
+            # raise Exception("No data by that SSID")
+
         response = ollama.embeddings(prompt=prompt, model="mxbai-embed-large")
         results = self.SSID_vectordatabase[SSID].query(
             query_embeddings=[response["embedding"]], n_results=n_results
         )
         # print(response)
         return results
+
+    def delete_data(self, SSID):
+        try:
+            self.client.delete_collection(SSID)
+        except ValueError:
+            print("ERROR: no data in the current session")
+        if SSID in self.SSID_vectordatabase:
+            self.SSID_vectordatabase.pop(SSID)
 
 
 def main():
--- backend/chat_test.py
+++ backend/chat_test.py
@@ -1,6 +1,9 @@
 from llama_backend import Chat
+from chromadb.errors import InvalidCollectionException
 from Vector_database_backend import vector_data_base
 import os
+import PyPDF2
+import magic
 
 
 def main():
@@ -11,6 +14,7 @@ def main():
         help = """'E' to exit
 'D' to load document
 'C' to chat
+'!' to delete all previous uploaded file's data
 : """
         os.system("cls" if os.name == "nt" else "clear")
         match input(help):
@@ -19,10 +23,26 @@ def main():
                 break
             case "D":
                 try:
-                    with open(input("file location : ")) as file:
-                        database.vectorize_string(
-                            file.read(), session.SSID, input("Document name : ")
-                        )
+                    filename, file_extension = os.path.splitext(
+                        input("file location : ")
+                    )
+
+                    match file_extension:
+                        case ".txt":
+                            with open(filename + file_extension, "rb") as file:
+                                database.vectorize_string(
+                                    file.read(), session.SSID, input("Document name : ")
+                                )
+                        case ".pdf":
+                            with open(filename + file_extension, "r") as file:
+                                string = ""
+                                for i in PyPDF2.PdfReader(file).pages:
+                                    string += i.extract_text()
+                                database.vectorize_string(
+                                    string, session.SSID, input("Document name : ")
+                                )
+                        case _:
+                            raise ("Unkown filetype")
                 except FileNotFoundError:
                     print("file not found")
             case "C":
@@ -31,15 +51,25 @@ def main():
                 )
                 while True:
                     user_in = input("User : ")
-                    if user_in.lower() == "exit":
-                        break
-                    elif user_in[0] == "?":
-                        answer = session.send_message(
-                            user_in[1:], look_up=True, data_base=database
-                        )
-                    else:
-                        answer = session.send_message(user_in)
-                    print(f"Assistant : {answer}")
+                    try:
+                        if user_in.lower() == "exit":
+                            break
+                        elif user_in[0] == "?":
+
+                            answer = session.send_message(
+                                user_in[1:], look_up=True, data_base=database
+                            )
+                        else:
+                            answer = session.send_message(user_in)
+                        print(f"Assistant : {answer}")
+                    except InvalidCollectionException:
+                        print("No imported documents")
+            case "!":
+                if "y" == input("Are you sure? (N/y) : ")[0].lower():
+                    database.delete_data(SSID=session.SSID)
+            case _:
+                print("not a command")
+
         input("PRESS ENTER TO CONTINUE")
 
 
